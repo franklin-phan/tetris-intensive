51060425227d0cb6f2405609a48885443d53af1f
FRANKLIN_PHAN <franklin.phan123@gmail.com>
2020-10-21 13:12:57 -0500

    resume and pause implemented

--- src/reducers/game-reducer.js
+++ src/reducers/game-reducer.js
@@ -68,11 +68,11 @@ const gameReducer = (state = defaultState(), action) => {
   
       case RESUME:
   
-        return state
+        return { ...state, isRunning: true }
   
       case PAUSE:
   
-        return state
+        return { ...state, isRunning: false }
   
       case GAME_OVER:
   
