1e3454829661dfa7b0f0d114d5c433466e5a4793
FRANKLIN_PHAN <franklin.phan123@gmail.com>
2020-10-21 11:57:46 -0500

    added grid square

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import GridSquare from './components/GridSquare'
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1 className="App-title">Tetris Redux</h1>
+        <GridSquare color="1" />
       </header>
     </div>
   );
